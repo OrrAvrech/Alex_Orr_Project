de381154c34698070c7c4aa17ab91cfa7a5eeb96
Alexfinkelshtein <sashafinkelshtein@gmail.com>
2017-08-09 17:55:56 +0300

    Tetrapod_ICA + blinking movie
    FastICA algorithm is working with our data from Tetrapod function.
    A blinking movie was added for visualization.

--- FastICA/Tetrapod_ICA.m
+++ FastICA/Tetrapod_ICA.m
@@ -1,19 +1,24 @@
 clc; clear all; close all;
 %% Load linear combination from Tetrapod
-load('C:\Users\orrav\Documents\Technion\8th\Project\Alex_Orr_Project\TetrapodPSF\Sequence2.mat');
+load('..\TetrapodPSF\Sequence2.mat');
 
 %% Mixed Sources Matrix
 size_lincomb = size(Sequence2.LinearCombinations) ;
-y = zeros(size_lincomb(3),size_lincomb(1)*size_lincomb(2)) ;
+x = zeros(size_lincomb(3), size_lincomb(1)*size_lincomb(2)) ;
 for i=1:size_lincomb(3)
-    y(i,:) = reshape(Sequence2.LinearCombinations(:,:,i), 1, []) ;
+    x(i,:) = reshape(Sequence2.LinearCombinations(:,:,i), 1, []) ;
 end
-
+%% Movie
+for i=1:size_lincomb(3)
+    imagesc(Sequence2.LinearCombinations(:,:,i));
+    pause (1) ;
+end
 %% FastICA -- to obtain Mixing_mat and original sources
-[A,W] = fastica(y) ;
-s = W * y ;
-s_img_mat = zeros(size_lincomb(1), size_lincomb(2), size_lincomb(3));
-for j=1:size_lincomb(3)
+num_planes = 2;
+[A,W] = fastica(x, 'numOfIC', num_planes) ;
+s = W * x;
+s_img_mat = zeros(size_lincomb(1), size_lincomb(2), num_planes);
+for j=1:num_planes
     s_img_mat(:,:,j) = reshape(s(j,:), size_lincomb(1), size_lincomb(2));
     figure(j);
     imagesc(s_img_mat(:,:,j));
