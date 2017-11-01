c6b6fba1f668cbd6fa064cc33f99f2a23c77823c
Alex Finkelshtein <alex.finkelshtein@intel.com>
2017-11-01 19:21:38 +0200

    Tamnun gen func - wip
    not finished, file handling part is ready, need to adjust new variable type

--- DataSimulation/Tamnun_gen.m
+++ DataSimulation/Tamnun_gen.m
@@ -1,9 +1,8 @@
-function finished = Tamnun_gen(samp_num, save_to)
-    %inputs
+function finished = Tamnun_gen(samp_num, save_to_dir)
+        %%inputs
         %save_to string in '' not ""
-    %choose:
-    %samp_num = 100; %temp
-    
+        
+        %%choose:
     NumZplanes = 80;
     MaxSources = 5;
     x_center = 0;
@@ -12,33 +11,35 @@ function finished = Tamnun_gen(samp_num, save_to)
     y_width = 2e-7;
     
         %%helpers:
+        
     zVec = linspace(-2e-6,2e-6,NumZplanes+1);
-    %name_ind:
-    file_naming_ind = 1;
-%     [cur_path,cur_folder] = fileparts(pwd)
-    
-        %%make sure the folder exists
-    original_path = dir; % dir(name) returns file list containg name sub string
-    orig_listing = {original_path.name};
-    exists = find(ismember(orig_listing,save_to));
+        
+        %%file handling:
+        
+    file_naming_ind = 0;
+            %%make sure the folder exists
+    orig_listing = dir; % dir(name) returns file list containg name sub string
+    orig_listing = {orig_listing.name};
+    exists = find(ismember(orig_listing,save_to_dir));
     if ~numel(exists)
-        mkdir(save_to);
+        mkdir(save_to_dir);
     end
     
-    cdir(['..\',save_to,'\']); %change to saving destination
+    cdir([save_to_dir,'\']); %change to saving destination
+    
     dest_listing = dir
-    if ~dest_listing(end).isdir
+    if ~dest_listing(end).isdir %is empty?
         dest_names = ({dest_listing.name});
         last_file = dest_names{end};
-        last_file.isdir
         file_naming_ind = str2num(last_file(1:end-4)) %remove '.mat'
+    end
     
     
     for i=1:samp_num
         file_ind = file_naming_ind + i;
-        %stochastic:
+            %%stochastic:
         NumSources = randi(MaxSources);
-        %actual data:
+            %%actual data:
         x = (rand(1,NumSources)+x_center-0.5)*x_width/2;
         y = (rand(1,NumSources)+y_center-0.5)*y_width/2;
         z = (randi(NumZplanes,[1,NumSources]));
@@ -48,17 +49,18 @@ function finished = Tamnun_gen(samp_num, save_to)
         EmittersObj.zVec        = z;
         EmittersObj.NumSources  = NumSources; 
 
-        if flag_Save2File == 1  %%remove condition
-            m = matfile(['DataObjects',num2str(file_ind),'.mat'],'Writable',true);
-            try
-                m.EmittersObj = [m.EmittersObj, EmittersObj];
-            catch
-                save('DataObjects.mat', 'EmittersObj', '-v7.3');
-            end
-
-            whos('-file','DataObjects.mat')
+%         if flag_Save2File == 1  %%remove condition
+        m = matfile([num2str(file_ind),'.mat'],'Writable',true);
+        try
+            m.EmittersObj = [m.EmittersObj, EmittersObj];
+        catch
+            save([num2str(file_ind),'.mat'], 'EmittersObj', '-v7.3');
         end
-        original_path = cdir('..\DataSimulation\');
+
+        whos('-file',[num2str(file_ind),'.mat'])
+%         end
+        
+        original_path = cd('..\..\DataSimulation\'); %%cdir doesn't work not sure why
         Emitters = Objects.EmittersObj(1,ii);
         NumSources = Emitters.NumSources;
 
