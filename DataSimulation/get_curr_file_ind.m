276087a1a2a07041950689050c4595f2056addcf
Alex Finkelshtein <alex.finkelshtein@intel.com>
2017-12-05 22:44:23 +0200

    fix for file ind function

--- DataSimulation/get_curr_file_ind.m
+++ DataSimulation/get_curr_file_ind.m
@@ -14,12 +14,16 @@ function file_ind = get_curr_file_ind(save_to_dir,file_ind)
     if ~dest_listing(end).isdir %is empty?
         dest_names = ({dest_listing.name});
         dest_names = dest_names(~cellfun(@isempty,regexp(dest_names,'.mat'))); % filter files that don't end with .mat
-        last_file = dest_names{end};
-        file_naming_ind = str2num(last_file(1:end-4)); %remove '.mat'
+        if (numel(dest_names) == 0)
+            file_naming_ind = 0;
+        else
+            last_file = dest_names{end};
+            file_naming_ind = str2num(last_file(1:end-4)); %remove '.mat'
+        end
     end
-    if (numel(file_naming_ind) == 0)
-        file_naming_ind = 0;
-    end
+%     if (numel(file_naming_ind) == 0)
+%         file_naming_ind = 0;
+%     end
     file_ind = file_naming_ind ;
     cd(original_path)
 end
