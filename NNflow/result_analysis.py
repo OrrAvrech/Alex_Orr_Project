439e2b8721b74e04d0012e083f64d22f30acba5e
Alexfinkelshtein <sashafinkelshtein@gmail.com>
2018-04-05 19:18:20 +0300

    framework for analysis
    basic image plot for comparison added

--- NNflow/result_analysis.py
+++ NNflow/result_analysis.py
@@ -5,4 +5,47 @@ Created on Wed Apr  4 19:08:40 2018
 @author: Alexfi
 """
 
-#
+import net_output_gen as nog
+import os
+import numpy as np
+from dataset_NEWtf import load_dataset
+import matplotlib.pyplot as plt
+
+
+from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
+file_path = os.path.dirname(os.path.abspath(__file__))
+model_name = 'im64_f8_s4'
+ckpt_name = 'im64_f8_s4_2018-04-05_1801'
+ckpt_path = os.path.join(file_path,'checkpoints','ckpt_'+model_name,ckpt_name)
+
+dataObj, _, _, _ = load_dataset(1,2)
+
+#print(dataObj.train.features)
+#print(dataObj.test.features)
+#print(dataObj.validation.features)
+
+
+sample = dataObj.train.features
+label = dataObj.train.labels
+
+
+# List ALL tensors example output: v0/Adam (DT_FLOAT) [3,3,1,80]
+#print_tensors_in_checkpoint_file(file_name=ckpt_path , tensor_name='y_conv:0', all_tensors=False)
+
+
+estimated = nog.net_output_gen('reshape_y/y_conv:0',['x:0','dropout/keep_prob:0'],[sample, 0.5], ckpt_path, 'name')
+
+#### y_conv analysis
+#estimated = estimated[0, :, :, :]
+#plt.figure(1)
+#plt.imshow(estimated)
+
+fig = plt.figure(1)
+ax = plt.subplot(221)
+ax.set_title('estimated')
+plt.imshow(estimated[0, :, :, 1])
+ax = plt.subplot(222)
+ax.set_title('label')
+plt.imshow(label[0, :, :, 1])
+
+
